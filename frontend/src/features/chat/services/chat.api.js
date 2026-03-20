2a5dde18cc783d3d2bddd6f6cb9367afaf80774c
Yugant Singh <yugantsingh9651@gmail.com>
2026-03-20 16:30:50 +0530

    url changes

--- frontend/src/features/chat/services/chat.api.js
+++ frontend/src/features/chat/services/chat.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "https://querynest-gh05.onrender.com/",
+  baseURL: "https://querynest-gh05.onrender.com",
   withCredentials: true
 })
 
